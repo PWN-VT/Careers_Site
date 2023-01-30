7abbe6a20fca2601dad37ceac7c6141e39810e30
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2023-01-30 15:21:45 -0500

    fixes bugs

--- project/__init__.py
+++ project/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager 
+from flask_login import LoginManager
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
