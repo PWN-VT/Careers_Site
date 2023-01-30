2a15a61e927c9dbe575b032c8dc3f19191df72a7
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2023-01-30 16:37:47 -0500

    fixes

--- project/main.py
+++ project/main.py
@@ -29,7 +29,7 @@ def explore():
 @login_required
 def explore_id(id):
     user = User.query.get(id)
-    if user.public == 1 and user.student == 0:
+    if user.public == '1' and user.student == '0':
         return render_template('explore_id.html', name=user.name, email=user.email, major=user.major, jobTitle=user.jobTitle, company=user.company, location=user.location, phone=user.phone, website=user.website, linkedin=user.linkedin, twitter=user.twitter)
     else:
         return render_template('explore.html', name=current_user.name, email=current_user.email, major=current_user.major)
