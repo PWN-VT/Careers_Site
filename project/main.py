43924736d065f94fd2ad2a45cf12c9855cfe35a7
S1n1st3r <S1n1st3r@gmail.com>
2023-02-01 10:27:15 -0500

    Added favicon.ico

--- project/main.py
+++ project/main.py
@@ -1,5 +1,5 @@
 # main.py
-import sys
+import os
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from .models import User
@@ -9,6 +9,11 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+#add favicon
+@main.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'templates', 'favicon.ico'),
 
 @main.route('/profile')
 @login_required
