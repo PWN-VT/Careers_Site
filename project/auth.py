eedefeada0055d6c01dccdbb64f0bc8a55d1f3f9
gsmith257-cyber <55564824+gsmith257-cyber@users.noreply.github.com>
2023-02-10 09:27:08 -0500

    bug fix

--- project/auth.py
+++ project/auth.py
@@ -1,7 +1,8 @@
 # auth.py
 
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash, secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
